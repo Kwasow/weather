58b54d739cea98a70bed700b9e31974381d9d9cf
Karol Wąsowski <wasowski02@protonmail.com>
2023-06-24 00:09:51 +0200

    Styled react-select

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -10,6 +10,53 @@ import {
 
 export const StyledSelector = styled(Select)`
   width: 20%;
+
+  .react-select__control {
+    background-color: ${props => props.theme.surface};
+  }
+
+  .react-select__control:hover {
+    border-color: ${props => props.theme.primary};
+  }
+
+  .react-select__control--is-focused {
+    border-color: ${props => props.theme.primary};
+  }
+
+  .react-select__input-container {
+    color: ${props => props.theme.onBackground}
+  }
+
+  .react-select__input-container {
+    color: ${props => props.theme.onBackground}
+  }
+
+  .react-select__single-value {
+    color: ${props => props.theme.onBackground}
+  }
+
+  .react-select__option {
+    color: ${props => props.theme.onBackground};
+  }
+
+  .react-select__option:hover {
+    background-color: ${props => props.theme.primarySelected};
+  }
+
+  .react-select__option--is-focused {
+    background-color: ${props => props.theme.primarySelected};
+  }
+
+  .react-select__option--is-selected {
+    background-color: ${props => props.theme.primarySelected};
+  }
+
+  .react-select__menu {
+    background-color: ${props => props.theme.surface};
+    color: ${props => props.theme.onSurface};
+    border: 1px solid ${props => props.theme.onBackgroundBorder};
+    border-opacity: 50%;
+  }
 `
 
 export function CitySelector() {
@@ -39,5 +86,6 @@ export function CitySelector() {
       } else {
         dispatch(resetLocation())
       }
-    }}/>
+    }}
+    classNamePrefix="react-select"/>
 }
