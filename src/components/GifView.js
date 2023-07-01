8bac49856b77089f2c46eca15fb785a90507197a
Karol Wąsowski <wasowski02@protonmail.com>
2023-07-01 15:34:50 +0200

    Added automatic gif switching

--- src/components/GifView.js
+++ src/components/GifView.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Video } from './index'
+import { interval } from 'rxjs'
+import { nextGif } from '../redux/reducers/weatherGifSlice'
 
 export function GifView() {
+  const dispatch = useDispatch()
   const gifs = useSelector(state => state.weatherGif.gifs)
   const currentIndex = useSelector(state => state.weatherGif.currentIndex)
+
+  useEffect(() => {
+    const timer = interval(1000 * 30)
+    const gifUpdater = timer.subscribe(() => dispatch(nextGif()))
+
+    return () => {
+      gifUpdater.unsubscribe()
+    }
+  })
 
   if (currentIndex === -1 || gifs.length === 0) {
     return <></>
--- src/redux/reducers/weatherGifSlice.js
+++ src/redux/reducers/weatherGifSlice.js
@@ -2,10 +2,13 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const WEATHER_GIF_ACTION = 'weatherGif'
 const ACTION_SET_GIFS = 'setGifs'
+const ACTION_NEXT_GIF = 'nextGif'
 const ACTION_RESET_GIFS = 'resetGifs'
 
 export const WEATHER_GIF_ACTION_SET =
   `${WEATHER_GIF_ACTION}/${ACTION_SET_GIFS}`
+export const WEATHER_GIF_ACTION_NEXT =
+  `${WEATHER_GIF_ACTION}/${ACTION_NEXT_GIF}`
 export const WEATHER_GIF_ACTION_RESET =
   `${WEATHER_GIF_ACTION}/${ACTION_RESET_GIFS}`
 
@@ -22,6 +25,9 @@ export const weatherGifSlice = createSlice({
       state.gifs = action.payload
       state.currentIndex = 0
     },
+    [ACTION_NEXT_GIF]: (state) => {
+      state.currentIndex = (state.currentIndex + 1) % state.gifs.length
+    },
     [ACTION_RESET_GIFS]: state => {
       state.gifs = []
       state.currentIndex = NO_GIFS
@@ -29,6 +35,6 @@ export const weatherGifSlice = createSlice({
   }
 })
 
-export const { setGifs, resetGifs } = weatherGifSlice.actions
+export const { setGifs, nextGif, resetGifs } = weatherGifSlice.actions
 
 export default weatherGifSlice.reducer
