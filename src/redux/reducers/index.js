83bfab454a23ac628c90b432ad6000f84368b0be
Karol Wąsowski <wasowski02@protonmail.com>
2023-07-03 19:06:33 +0200

    Improved naming

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,17 +2,17 @@ import { combineReducers } from '@reduxjs/toolkit'
 import themeReducer from './themeSlice'
 import locationReducer from './locationSlice'
 import weatherReducer from './weatherSlice'
-import tabSlice from './tabSlice'
-import requestCounterSlice from './requestCounterSlice'
-import weatherGifSlice from './weatherGifSlice'
+import tabReducer from './tabSlice'
+import requestCounterReducer from './requestCounterSlice'
+import weatherGifReducer from './weatherGifSlice'
 
 const rootReducer = combineReducers({
   theme: themeReducer,
   location: locationReducer,
   weather: weatherReducer,
-  tab: tabSlice,
-  requestCounter: requestCounterSlice,
-  weatherGif: weatherGifSlice
+  tab: tabReducer,
+  requestCounter: requestCounterReducer,
+  weatherGif: weatherGifReducer
 })
 
 export default rootReducer
